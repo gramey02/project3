66cd4f1d55e9606db190d7897aa0808adc6a982b
Grace Ramey <graceramey@pcd-ak2q05d-lt.ucsf.edu>
2022-01-24 20:32:39 -0800

    updated check_mst assertions

--- test/test_mst.py
+++ test/test_mst.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 from mst import Graph
 from sklearn.metrics import pairwise_distances
+from search import Graph
 
 
 def check_mst(adj_mat: np.ndarray, 
@@ -28,11 +29,29 @@ def check_mst(adj_mat: np.ndarray,
         return abs(a - b) < allowed_error
 
     total = 0
+    mst_edges = 0
     for i in range(mst.shape[0]):
         for j in range(i+1):
             total += mst[i, j]
+            if mst[i,j] != 0:
+                mst_edges +=1 #increase the number of edges in the mst by 1 if there is a nonzero value in the mst array
+                
+    #compare total and expected weights. If less than allowed_error, then test will pass
     assert approx_equal(total, expected_weight), 'Proposed MST has incorrect expected weight'
-
+    
+    #a minimum spanning tree (without cycles) should have (# vertices - 1) edges, so check this assertion below:
+    assert mst_edges==len(adj_mat) - 1 #length of adj_mat should be # vertices, so subtract 1 from this to get # of edges
+    
+    #check to make sure the adjacency and mst matrices are symmetric, since both are undirected
+    for i in range(adj_mat.shape[0]):
+        for j in range(i+1):
+            assert adj_mat[i,j] == adj_mat[j,i]
+    for i in range(mst.shape[0]):
+        for j in range(i+1):
+            assert mst[i,j] == mst[i,j]
+    
+    #minimum spanning trees are always connected--use a version of the breadth-first search algorithm from last week to check this
+    
 
 def test_mst_small():
     """ Unit test for the construction of a minimum spanning tree on a small graph """
@@ -59,4 +78,17 @@ def test_mst_single_cell_data():
 
 def test_mst_student():
     """ TODO: Write at least one unit test for MST construction """
+    #create a dummy networks to test on
+    adj_mat = np.array([[0, 20, 8, 0, 0, 0, 1],
+                   [20, 0, 0, 4, 0, 0, 10],
+                   [8, 0, 0, 6, 2, 5, 0],
+                   [0, 4, 6, 0, 0, 0, 0],
+                   [0, 0, 2, 0, 0, 0, 0],
+                   [0, 0, 5, 0, 0, 0, 0],
+                   [1, 10, 0, 0, 0, 0, 0]
+                   ])
+    check
+    
+    #create another dummy network that is an edge case, i.e. that has a cycle in it
+    
     pass
